af667284355a39a9daaaa2f23ee32871fb992fc1
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-14 13:23:50 -0300

    Component Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@chakra-ui/react'
 import Header from '../components/Header/indes'
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
     <Flex direction="column">
       <Header />
+      <Banner />
     
 
       <main>
