6ad5587a466769b6e33b8f2dabdfc95afda8ffd3
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-14 13:50:42 -0300

    Menu

---
+++ src/components/Menus/MenuItem.tsx
@@ -0,0 +1,33 @@
+import { useBreakpointValue, Flex, Text, Image } from "@chakra-ui/react"
+
+interface MenuItemProps {
+  icon: string;
+  text: string;
+}
+
+export default function MenuItem({icon, text}: MenuItemProps ) {
+  // verificar se está no modo Mobile - isMobile
+  const isMobile = useBreakpointValue({
+    base:false,
+    sm:true
+  })
+
+  return (
+      <Flex 
+        direction={["row","column"]} 
+        align="center" 
+        justify="center">
+        {isMobile ? 
+          <Image 
+            src={`/icons/${icon}.svg`} 
+            w="85px" 
+            h="85px" 
+            mb="6" /> : 
+            <Text color="yellow.400" fontSize="4xl" mr="2">•</Text>}
+        <Text 
+          fontWeight="600" 
+          color="gray.700" 
+          fontSize={["md", "xl","2xl"]}>{text}</Text>
+      </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { Flex } from '@chakra-ui/react'
 import Header from '../components/Header/indes'
 import Banner from '../components/Banner'
+import Menus from '../components/Menus'
 
 export default function Home() {
   return (
     <Flex direction="column">
       <Header />
       <Banner />
+      <Menus />
     
 
       <main>
